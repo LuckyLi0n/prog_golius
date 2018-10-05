f6463f56b5c498c10242f92de9f037de89da290b
LuckyLi0n <Rita8996@mail.ru>
2018-10-05 14:21:51 +0300

    Создана папка Turtle

---
+++ Turtle/turtle_14_11.py
@@ -0,0 +1,5 @@
+import turtle
+turtle.shape("turtle")
+for x in range(11):
+    turtle.forward(100)
+    turtle.left(180+180/11)
---
+++ Turtle/turtle_8.py
@@ -0,0 +1,6 @@
+import turtle
+N = 10
+for i in range(50):
+    turtle.forward(N)
+    turtle.left(90)
+    N += 10
